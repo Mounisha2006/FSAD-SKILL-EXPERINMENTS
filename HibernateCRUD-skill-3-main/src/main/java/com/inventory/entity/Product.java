2b4ac9790a5179b249aa277168c7a7d00cf48e9a
Mounisha2006 <Lakshmimounisha835@gmail.com>
2026-04-07 09:47:48 +0530

    Add files via upload

---
+++ HibernateCRUD-skill-3-main/src/main/java/com/inventory/entity/Product.java
@@ -0,0 +1,32 @@
+package com.inventory.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int productId;
+
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+
+    public Product() {}
+
+    public Product(String name, String description, double price, int quantity) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // getters & setters
+}
